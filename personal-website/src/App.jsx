66f9a45e00ba88d6cc822cf6f0e5cb1f7c34d4fd
Corey Taylor <taylo278@cougars.csusm.edu>
2023-12-27 17:21:46 -0800

    Updated website w placeholders

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,6 +1,18 @@
 import React from "react";
-import "./styles/main.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+
+// Your other components here, e.g., About, Dashboard, etc.
 
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add other nested routes here */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      </Routes>
+    </Router>
+  );
 }
--- personal-website/vite.config.js
+++ personal-website/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {},
+  resolve: {
+    alias: {},
   },
+  css: {
+    postcss: {
+      plugins: [tailwind, autoprefixer],
+    },
+  },
+  server: {},
 });
