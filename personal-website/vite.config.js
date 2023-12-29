7015ab5112d53e11b8dc94c26cefe2fb9771b4e0
Corey Taylor <taylo278@cougars.csusm.edu>
2023-12-28 16:46:59 -0800

    Working version, profile picture and projects

--- personal-website/vite.config.js
+++ personal-website/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vue()],
   resolve: {
     alias: {},
   },
