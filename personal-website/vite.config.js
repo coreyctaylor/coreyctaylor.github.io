c4aee7dcb407097e1c47ce89e1b200fb7aa4d137
Corey Taylor <taylo278@cougars.csusm.edu>
2023-12-29 15:35:05 -0800

    C

--- personal-website/vite.config.js
+++ personal-website/vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/personal-project/",
+  base: "/personal-website",
   jsx: "react",
   plugins: [react(), vue()],
   resolve: {
