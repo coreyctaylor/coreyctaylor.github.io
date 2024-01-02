478db506f5300f6e96f38e4e4a0eeeb099fafceb
Corey Taylor <taylo278@cougars.csusm.edu>
2024-01-01 16:38:23 -0800

    Added website to project, styling

--- personal-website/src/components/home.jsx
+++ personal-website/src/components/home.jsx
@@ -4,6 +4,7 @@ import resumePDF from "../assets/resume.pdf";
 import ProUMLDashboardPage from "../assets/ProUMLDashboardPage.png";
 import ACUDashboardPage from "../assets/ACUDashboardPage.png";
 import ProfilePicture from "../assets/PFP2.png";
+import WebsitePicture from "../assets/WebsitePicture.png";
 import { Form } from "../components/form";
 
 import {
@@ -197,6 +198,104 @@ export default function Home() {
                   My Projects
                 </h2>
               </div>
+              {/* Portfolio */}
+              <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none shadow-2xl ring-black ring-opacity-10 bg-gray-100">
+                <div className="p-8 sm:p-10 lg:flex-auto">
+                  <h3 className="text-2xl font-bold tracking-tight text-gray-900">
+                    Corey Taylor's Website
+                  </h3>
+                  <p className="mt-6 text-base leading-7 text-gray-600">
+                    This website was created using ReactJS, Vite, and
+                    TailwindCSS. It is a single-page application that is hosted
+                    on Github Pages and uses a custom domain name.
+                  </p>
+                  <div className="mt-10 flex items-center gap-x-4">
+                    <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
+                      Tools used in this project:
+                    </h4>
+                    <div className="h-px flex-auto bg-gray-100"></div>
+                  </div>
+                  <ul
+                    role="list"
+                    className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+                  >
+                    <li className="flex gap-x-3">
+                      <svg
+                        className="h-6 w-5 flex-none text-indigo-600"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      ReactJS
+                    </li>
+                    <li className="flex gap-x-3">
+                      <svg
+                        className="h-6 w-5 flex-none text-indigo-600"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      Vite
+                    </li>
+                    <li className="flex gap-x-3">
+                      <svg
+                        className="h-6 w-5 flex-none text-indigo-600"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      TailwindCSS
+                    </li>
+                    <li className="flex gap-x-3">
+                      <svg
+                        className="h-6 w-5 flex-none text-indigo-600"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      EmailJS
+                    </li>
+                  </ul>
+                </div>
+                <div className="-mt-2 py-8 mx-auto lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 text-center lg:mr-10">
+                  <img
+                    className="inline-block mt-8 rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
+                    src={WebsitePicture}
+                    alt="Website Picture"
+                  />
+                  <a
+                    href="https://coreyctaylor.com/" // Replace with your desired external link
+                    rel="noopener noreferrer" // Recommended for security when using target="_blank"
+                    className="mt-5 mb-8 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transform transition-transform duration-300 hover:scale-105"
+                  >
+                    View this project
+                  </a>
+                </div>
+              </div>
               {/* ACU */}
               <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none shadow-2xl ring-black ring-opacity-10 bg-gray-100">
                 <div className="p-8 sm:p-10 lg:flex-auto">
@@ -282,7 +381,7 @@ export default function Home() {
                     </li>
                   </ul>
                 </div>
-                <div className="-mt-2 py-8 mx-auto lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 text-center">
+                <div className="-mt-2 py-8 mx-auto lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 text-center lg:mr-10">
                   <img
                     className="inline-block mt-8 rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
                     src={ACUDashboardPage}
@@ -381,9 +480,9 @@ export default function Home() {
                     </li>
                   </ul>
                 </div>
-                <div className="-mt-2 py-8 mx-auto lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 text-center">
+                <div className="-mt-2 py-8 mx-auto lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 text-center lg:mr-10">
                   <img
-                    className="inline-block mt-4 rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
+                    className="inline-block mt-8 rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
                     src={ProUMLDashboardPage}
                     alt="ProUML Dashboard Page"
                   />
@@ -391,7 +490,7 @@ export default function Home() {
                     href="https://prouml.com/" // Replace with your desired external link
                     target="_blank" // Open the link in a new tab
                     rel="noopener noreferrer" // Recommended for security when using target="_blank"
-                    className="mt-5 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transform transition-transform duration-300 hover:scale-105"
+                    className="mt-5 mb-8 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transform transition-transform duration-300 hover:scale-105"
                   >
                     View this project
                   </a>
